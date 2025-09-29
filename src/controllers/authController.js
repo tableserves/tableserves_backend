a17e0c850d4803f1189aec5420167e4ae29fa7bd
Dinesh020400 <dineshhhkumar54321@gmail.com>
2025-09-29 12:03:15 +0530

    emil url

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -812,7 +812,7 @@ const requestPasswordReset = catchAsync(async (req, res) => {
 
 Please click on the following link, or paste this into your browser to complete the process:
 
-http://${req.headers.host}/reset/${resetToken.token}
+${process.env.FRONTEND_URL}/reset/${resetToken.token}
 
 If you did not request this, please ignore this email and your password will remain unchanged.
 `,
